16fc274417e15ba2df9a095ef4c205cb0e48165c
Martín Ferreira <pablo.ferreira@dxc.com>
2018-11-21 23:00:27 -0300

    Arduino cambios Nahu

--- Arduino/analogico_serial/analogico_serial.ino
+++ Arduino/analogico_serial/analogico_serial.ino
@@ -1,13 +1,36 @@
 const int pinJoyX = A0;
 const int pinJoyY = A1;
-const int pinJoyButton = 2;
+const int pinJoyButton = 10;
+
+
+const int a = 2;
+const int b = 3;
+const int c = 4;
+const int d = 5;
+const int e = 6;
+const int f = 7;
+const int g = 8;
  
 void setup() {
    pinMode(pinJoyButton , INPUT_PULLUP);   //activar resistencia pull up 
-   Serial.begin(9600);
+    pinMode(a, OUTPUT);
+    pinMode(b, OUTPUT);
+    pinMode(c, OUTPUT);
+    pinMode(d, OUTPUT);
+    pinMode(e, OUTPUT);
+    pinMode(f, OUTPUT);
+    pinMode(g, OUTPUT);
+   Serial.begin(100000);
 }
  
 void loop() {
+   envioCoordenadas();
+    delay(100);
+    //mostrarDisplay();
+    //delay(100);
+}
+
+void envioCoordenadas(){
    int x = 0;
    int y = 0;
    bool botonPresionado = false;
@@ -22,4 +45,115 @@ void loop() {
    Serial.print(",");
    Serial.println(botonPresionado);
    delay(100);
+  
+}
+
+
+
+void mostrarDisplay(){
+
+  String contador = Serial.readString();
+  if(contador == "0"){
+    digitalWrite(a, LOW);
+    digitalWrite(b, LOW);
+    digitalWrite(c, LOW);
+    digitalWrite(d, LOW);
+    digitalWrite(e, LOW);
+    digitalWrite(f, LOW);
+    digitalWrite(g, HIGH);
+  }
+  else{
+    int contadorInt = contador.toInt();
+    switch (contadorInt){
+    case 1:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, LOW);
+      digitalWrite(c, LOW);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, HIGH);
+      break;
+      
+    case 2:
+      digitalWrite(a, LOW);
+      digitalWrite(b, LOW);
+      digitalWrite(c, HIGH);
+      digitalWrite(d, LOW);
+      digitalWrite(e, LOW);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, LOW);
+      break;
+      
+    case 3:
+      digitalWrite(a, LOW);
+      digitalWrite(b, LOW);
+      digitalWrite(c, LOW);
+      digitalWrite(d, LOW);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, LOW);
+      break;
+      
+    case 4:
+      digitalWrite(a, HIGH);
+      digitalWrite(b, LOW);
+      digitalWrite(c, LOW);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, LOW);
+      digitalWrite(g, LOW);
+      break;
+      
+    case 5:
+      digitalWrite(a, LOW);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, LOW);
+      digitalWrite(d, LOW);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, LOW);
+      digitalWrite(g, LOW); 
+       break;
+       
+    case 6:
+      digitalWrite(a, LOW);
+      digitalWrite(b, HIGH);
+      digitalWrite(c, LOW);
+      digitalWrite(d, LOW);
+      digitalWrite(e, LOW);
+      digitalWrite(f, LOW);
+      digitalWrite(g, LOW);
+     break;
+     
+    case 7:
+      digitalWrite(a, LOW);
+      digitalWrite(b, LOW);
+      digitalWrite(c, LOW);
+      digitalWrite(d, HIGH);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, HIGH);
+      digitalWrite(g, HIGH);
+      break;
+      
+    case 8:
+      digitalWrite(a, LOW);
+      digitalWrite(b, LOW);
+      digitalWrite(c, LOW);
+      digitalWrite(d, LOW);
+      digitalWrite(e, LOW);
+      digitalWrite(f, LOW);
+      digitalWrite(g, LOW);
+      break;
+      
+    case 9:
+      digitalWrite(a, LOW);
+      digitalWrite(b, LOW);
+      digitalWrite(c, LOW);
+      digitalWrite(d, LOW);
+      digitalWrite(e, HIGH);
+      digitalWrite(f, LOW);
+      digitalWrite(g, LOW);
+      break;
+      }
+  }
 }
